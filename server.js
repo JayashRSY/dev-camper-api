15ed852d9d1fd17c511167c29512934be4235d26
Jayash Raj Singh Yadav <65907404+JayashRSY@users.noreply.github.com>
2023-09-19 14:35:42 +0530

    rate limiter, cors, hpp

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const cookieParser = require('cookie-parser')
 const mongoSanitize = require('express-mongo-sanitize')
 const helmet = require('helmet')
 const xss = require('xss-clean')
-var cors = require('cors')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
+const cors = require('cors')
 const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 // var cors = require('cors')
@@ -45,6 +47,16 @@ app.use(mongoSanitize());
 app.use(helmet());
 // Prevent XSS attacks
 app.use(xss());
+// Rate Limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 100
+});
+app.use(limiter);
+// Prevent http param pollution
+app.use(hpp());
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
